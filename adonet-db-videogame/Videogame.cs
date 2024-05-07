a9b69732417bd3be40eb8f328132d66d93d407bb
DorinVieru <dorino.vieru@gmail.com>
2024-05-07 14:47:26 +0200

    UP: Create class of videogame & public construct

---
+++ adonet-db-videogame/Videogame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adonet_db_videogame
+{
+    public class Videogame
+    {
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public string ReleaseDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int SoftwareHouseID { get; set; }
+
+        // Costruttore
+        public Videogame(string name, string overview, string release_date, DateTime createdAt, DateTime updatedAt, int software_hause_id)
+        {
+            Name = name;
+            Overview = overview;
+            ReleaseDate = release_date;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            SoftwareHouseID = software_hause_id;
+        }
+    }
+
+    
+}
